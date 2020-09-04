c8458a34890e3a9c73ca0ee0b2afb60e62b7ef40
Saif-Haider <saifhaider0z@gmail.com>
2020-09-05 00:32:17 +0530

    2ndt commit

--- src/main/java/com/haider/app/ws/ui/controller/UserController.java
+++ src/main/java/com/haider/app/ws/ui/controller/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.haider.app.ws.ui.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("users")
@@ -18,7 +21,7 @@ public class UserController {
 	}
 
 	@PostMapping
-	public String createUser()
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails)
 	{
 		 return "create user was called";
 	}
