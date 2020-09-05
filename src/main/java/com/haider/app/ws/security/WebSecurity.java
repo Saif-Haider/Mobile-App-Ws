0a709c9a15ea223fd9c73908fc9ebe04d71049aa
Saif-Haider <saifhaider0z@gmail.com>
2020-09-05 23:07:35 +0530

    9th commit

--- src/main/java/com/haider/app/ws/security/WebSecurity.java
+++ src/main/java/com/haider/app/ws/security/WebSecurity.java
@@ -12,7 +12,7 @@ import com.haider.app.ws.service.UserService;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-	private final UserService userDetailsService ;
+	private final UserService userDetailsService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
@@ -20,16 +20,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 
 	}
-	
+
 	// "/users" IS PERMITTED TO ALL & ANY OTHER REQUEST NEEDS AUTHENTICATION
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-		.antMatchers(HttpMethod.POST,"/users")
-		.permitAll()
-		.anyRequest();
+		.antMatchers(HttpMethod.POST, "/users").permitAll().anyRequest()
+		.authenticated();
 	}
-	
+
 	// PASSING USERDETAILSSERVICES & THE ENCRYPTION METHOD WE ARE USING
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
