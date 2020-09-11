04aeba563835ad679c7c764eb65b42fe2f236139
Saif-Haider <saifhaider0z@gmail.com>
2020-09-11 20:47:33 +0530

    22.1th commit

--- src/main/java/com/haider/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/haider/app/ws/MobileAppWsApplication.java
@@ -5,29 +5,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+// SpringBootServletInitializer extended for creating a war
+
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication /* extends SpringBootServletInitializer */{
+
+	// Added to create a war
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(MobileAppWsApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
-	
-	
-	// CREATING BEAN FOR AUTOWIRING 
+
+	// CREATING BEAN FOR AUTOWIRING
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-    
+
 	// Need a bean so otherwise CONTEXT won't be initialized by spring
 //	@Bean
 //	public SpringApplicationContext springApplicationContext() {
 //		return new SpringApplicationContext();
 //	}
-	
+
 //	@Bean(name ="AppProperties")
 //	public AppProperties appProperties() {
 //		return new AppProperties();
 //	}
 }
-
